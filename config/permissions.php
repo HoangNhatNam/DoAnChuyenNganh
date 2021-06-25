cb244e7cc8399966b1281a502395e66d109c7c1b
HoangNhatNam <65563306+HoangNhatNam@users.noreply.github.com>
2021-06-25 19:18:18 +0700

    Add files via upload

---
+++ config/permissions.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'access' => [
+        'list-regent' => 'regent_list',
+        'list-certificate' => 'certificate_list'
+    ],
+    'table_model' => [
+        'user',
+        'report',
+        'reportfile',
+        'setting',
+        'role',
+        'partner',
+        'level',
+        'certificate',
+        'regent',
+        'callforpaper',
+        'time'
+    ],
+    'module_childrent' => [
+        'list',
+        'add',
+        'edit',
+        'delete',
+        'download',
+        'profile'
+    ]
+];
